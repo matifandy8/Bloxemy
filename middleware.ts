18f3e1623850c8b42530a020fca7de9f9359b41f
Matias Fandiño <matifandy@gmail.com>
2025-07-11 18:45:29 -0300

    fix: Extend Content Security Policy in middleware to include CDN for styles

--- middleware.ts
+++ middleware.ts
@@ -7,7 +7,7 @@ export function middleware(request: NextRequest) {
   const csp = [
     `default-src 'self'`,
     `script-src 'self' 'nonce-${nonce}' https://cdn.jsdelivr.net`,
-    `style-src 'self' 'unsafe-inline' https://fonts.googleapis.com`,
+    `style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdn.jsdelivr.net`,
     `font-src 'self' https://fonts.gstatic.com`,
     `img-src 'self' data:`,
     `object-src 'none'`,
