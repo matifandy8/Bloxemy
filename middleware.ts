5708ec321021f956c17ef2d94f9078f910f3f19c
Matias Fandiño <matifandy@gmail.com>
2025-07-11 18:42:32 -0300

    fix: Update Content Security Policy in middleware

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,7 @@ export function middleware(request: NextRequest) {
 
   const csp = [
     `default-src 'self'`,
-    `script-src 'self' 'nonce-${nonce}'`,
+    `script-src 'self' 'nonce-${nonce}' https://cdn.jsdelivr.net`,
     `style-src 'self' 'unsafe-inline' https://fonts.googleapis.com`,
     `font-src 'self' https://fonts.gstatic.com`,
     `img-src 'self' data:`,
